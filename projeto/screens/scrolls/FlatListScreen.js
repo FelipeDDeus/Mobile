785e7858035789ec74628b1d36261431922a06bc
Felipe de Deus <131199783+FelipeDDeus@users.noreply.github.com>
2026-04-15 13:42:32 +0000

    ativi

--- projeto/screens/scrolls/FlatListScreen.js
+++ projeto/screens/scrolls/FlatListScreen.js
@@ -4,7 +4,7 @@ import { FlatList, Text } from 'react-native';
 export default function FlatListScreen() {
   return (
     <FlatList
-      data={[...Array(10001).keys()]}
+      data={[...Array(101).keys()]}
       renderItem={({ item }) => <Text>Item {item}</Text>}
       keyExtractor={(item) => item.toString()}
     />
--- projeto/screens/scrolls/ScrollViewScreen.js
+++ projeto/screens/scrolls/ScrollViewScreen.js
@@ -4,7 +4,7 @@ import { ScrollView, Text } from 'react-native';
 export default function ScrollViewScreen() {
   return (
     <ScrollView>
-      {Array.from({ length: 10001 }).map((_, i) => (
+      {Array.from({ length: 101 }).map((_, i) => (
         <Text key={i}>Item {i}</Text>
       ))}
     </ScrollView>
